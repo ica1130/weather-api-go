b967e84d9cfc8c693a1462b5d4ef52cf91e3808d
ica1130 <ilijakrilovic@gmail.com>
2024-11-05 12:41:30 +0100

    feat: implemented healtheck route.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,7 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/weather", app.weatherHandler)
 
 	return router

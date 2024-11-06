6796eee41b8ae7a2bd2c585c67bbf0f04b26c203
ica1130 <ilijakrilovic@gmail.com>
2024-11-06 23:57:36 +0100

    feat: created service layer.

--- cmd/api/weather.go
+++ cmd/api/weather.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"weather-api-go.ilijakrilovic.net/internal/data"
 )
 
 func (app *application) weatherHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "get weather data")
+	var weather data.Weather
+
+	err := app.readJSON(w, r, &weather)
+	if err != nil {
+		http.Error(w, "error occurred", http.StatusInternalServerError)
+	}
+
+	fmt.Fprintf(w, "%+v\n", weather)
 }

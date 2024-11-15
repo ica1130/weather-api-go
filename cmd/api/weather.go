baa23aaf004e138da34c8357ae5797108bd9a3bf
ica1130 <ilijakrilovic@gmail.com>
2024-11-15 13:11:41 +0100

    refactor: moved logic for caching to service layer from handler.

--- cmd/api/weather.go
+++ cmd/api/weather.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/redis/go-redis/v9"
-	"weather-api-go.ilijakrilovic.net/internal/data"
 	"weather-api-go.ilijakrilovic.net/internal/service"
 )
 
@@ -20,54 +15,16 @@ func (app *application) weatherHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ctx := r.Context()
-
-	cachedData, err := app.redis.Get(ctx, city).Result()
-
-	if err == redis.Nil {
-
-		weather, err := service.GetWeather(city)
-		if err != nil {
-			app.logger.Println(err)
-			http.Error(w, fmt.Sprintf("error retrieving weather: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		weatherJSON, err := json.Marshal(weather)
-
-		if err != nil {
-			app.logger.Println("error marshaling weather:", err)
-			http.Error(w, "server error", http.StatusInternalServerError)
-			return
-		}
-
-		err = app.redis.Set(ctx, city, weatherJSON, 10*time.Minute).Err()
-		if err != nil {
-			app.logger.Println("error saving data to redis:", err)
-			http.Error(w, "server error", http.StatusInternalServerError)
-			return
-		}
-		app.logger.Printf("new city added to redis: %s", city)
-		app.writeJSON(w, http.StatusOK, weather, nil)
-
-	} else if err != nil {
-
-		app.logger.Println(err)
-		http.Error(w, "error retrieving cached data", http.StatusInternalServerError)
-		return
-
-	} else {
-
-		var weather data.Weather
-
-		err = json.Unmarshal([]byte(cachedData), &weather)
-
-		if err != nil {
-			app.logger.Println("Error unmarshaling cached data:", err)
-			http.Error(w, "server error", http.StatusInternalServerError)
-			return
-		}
-		app.logger.Printf("city found: %s", city)
-		app.writeJSON(w, http.StatusOK, weather, nil)
+	weatherService := service.WeatherService{
+		RedisClient: app.redis,
 	}
+
+	weather, err := weatherService.GetWeatherWithCache(r.Context(), city)
+	if err != nil {
+		app.logger.Println("error:", err)
+		http.Error(w, "error retrieving weather", http.StatusBadRequest)
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, weather, nil)
 }
--- internal/service/weather.go
+++ internal/service/weather.go
@@ -1,13 +1,49 @@
 package service
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/redis/go-redis/v9"
 	"weather-api-go.ilijakrilovic.net/internal/data"
 )
+
+type WeatherService struct {
+	RedisClient *redis.Client
+}
+
+func (w WeatherService) GetWeatherWithCache(ctx context.Context, city string) (*data.Weather, error) {
+
+	var weather *data.Weather
+	expiration := 10 * time.Minute
+
+	cachedData, err := w.RedisClient.Get(ctx, city).Result()
+
+	if err == redis.Nil {
+		weather, err := GetWeather(city)
+		if err != nil {
+			return weather, err
+		}
+
+		weatherJSON, err := json.Marshal(weather)
+		if err != nil {
+			return weather, err
+		}
+
+		err = w.RedisClient.Set(ctx, city, weatherJSON, expiration).Err()
+		return weather, err
+	} else if err != nil {
+		return weather, err
+	} else {
+		err = json.Unmarshal([]byte(cachedData), &weather)
+		return weather, err
+	}
+
+}
 
 func GetWeather(city string) (*data.Weather, error) {
 
